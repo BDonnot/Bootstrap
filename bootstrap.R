a5b2ff2010064b6d74d3eb9a241a485d64006e32
Benjamin DONNOT <benjamin.donnot@gmail.com>
2015-04-21 16:55:15 +0200

    adding papers, continuing the code

--- bootstrap.R
+++ bootstrap.R
@@ -1,8 +1,10 @@
 # install.packages("actuar")
 library(actuar)
+library(e1071) 
+
 
 ##shape
-shape = 1
+scale = 1
 
 ##basic functions for bootstraping
 naif = function(data)
@@ -15,29 +17,38 @@ smooth = function(data,sd = 0.1)
   naif(data)+rnorm(length(data),0,sd=sd)
 }
 
-param = function(data,hat_scale)
+param = function(data,hat_shape)
 {
-  rpareto(length(data),shape = shape, scale = hat_scale )
+  rpareto(length(data),shape = hat_shape, scale = scale )+1
 }
 
 empiricQuantile = function(data,probs)
 {
-  quantile(data,probs = probs)
+  quantile(data,probs = probs,type = 1)
 }
 
-invFun = function(hat_scale,probs)
+invFun = function(hat_shape,probs)
 {
-  qpareto(probs,shape = shape, scale = hast_scale)
+  qpareto(probs,shape = hat_shape, scale = scale)+1
 }
 
-getParamHill = function(data)
+getParamEMV = function(data)
 {
   #return the parametric estimation of the coefficients shape and scale
   res = list()
   betaHat = 1/mean(log(data))
-  res$hast_scale = betaHat**2
+  res$hat_shape = betaHat
   return(res)
 }
+
+getParamHill = function(data,k = 10)
+{
+  dataS = log(sort(data,decreasing = T)[1:k])
+  res = list()
+  res$hat_shape = 1/(mean(dataS[1:(k-1)]) - dataS[k])
+  return(res)
+}
+
 ##more evoluate functions
 
 basicParam = function(data,NsBootstrap,probs,funBoot)
@@ -75,14 +86,57 @@ smoothParam = function(data,NsBootstrap,probs,sdSmooth = 0.1)
   return(res)
 }
 
+paramBootstrap = function(data,NsBootstrap,probs)
+{
+  shape_hat = getParamHill(data)$hat_shape
+  paramSpecifiq = function(x) 
+  {
+    param(x,shape_hat)
+  }
+  res = basicParam(data,NsBootstrap,probs,paramSpecifiq)
+  return(res)
+}
+
+asymptotiqueEst = function(data,probs)
+{
+  dataSorted = sort(data)
+  n = length(data)
+  nalpha = qnorm(c(.975),0,1)
+  i1 = round(n*probs[1]-sqrt(n)*nalpha*sqrt(probs[1]*(1-probs[1])),0 )
+  i2 = round(n*probs[1]+sqrt(n)*nalpha*sqrt(probs[1]*(1-probs[1])),0 )
+  res = list()
+  res$min = dataSorted[i1]
+  res$max = dataSorted[i2]
+  return(res)
+}
 
 #les donnees
-x = rpareto(30,shape=shape,scale = 1)
+shape = 2
+x = rpareto(30,shape=shape,scale = scale)+1
 
-naifParam(x,1000,c(0.01,0.99))
-smoothParam(x,1000,c(0.01,0.99),0.1)
+probs = c(.5,.6,.7,.75,.90,.95,.99)
+qpareto(probs,shape = shape, scale = 1)+1
+naifParam(x,10000,probs)
+smoothParam(x,10000,probs,log(length(x))/length(x) )
+smoothParam(x,10000,probs,1/sqrt(length(x)) )
+paramBootstrap(x,10000,probs,1/sqrt(length(x)) )
+qpareto(probs,shape = shape, scale = 1)+1
 
 
-x = rpareto(10000000,shape=1,scale = 2)
-betaHat = (1/mean(log(x)))**2
-betaHat
+p = .75
+asymptotiqueEst(x,p)
+
+c(quantile(x,p)-(p*(1-p))/dpareto(qpareto(p,shape = shape, scale = scale),shape = shape, scale = scale)**2*1.96/sqrt(length(x))
+  ,quantile(x,p)+(p*(1-p))/dpareto(qpareto(p,shape = shape, scale = scale),shape = shape, scale = scale)**2*1.96/sqrt(length(x))
+)
+
+
+
+x = rpareto(30000,shape=2,scale =1)+1
+getParamEMV(x)
+getParamHill(x,sqrt(length(x)))
+
+# x = rpareto(10000000,shape=12,scale =1)+1
+# betaHat = 1/mean(log(x))
+# betaHat
+# hist(x)
