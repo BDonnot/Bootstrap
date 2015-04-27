461aac47883481222c4f83463e02f20e542b9f0d
BDonnot <benjamin.donnot@gmail.com>
2015-04-27 19:00:29 +0200

    coding all the functions needed

--- bootstrap.R
+++ bootstrap.R
@@ -3,7 +3,7 @@ library(actuar)
 library(e1071) 
 
 
-##shape
+##assign the scale (unchanged)
 scale = 1
 
 ##basic functions for bootstraping
@@ -50,14 +50,13 @@ getParamHill = function(data,k = 10)
 }
 
 ##more evoluate functions
-
-basicParam = function(data,NsBootstrap,probs,funBoot)
+basicParam = function(data,NsBootstrap,probs,funBoot,funparam=empiricQuantile)
 {
   #on simule les echantillons bootstrap (naivement)
   matBoot = sapply(1:NsBootstrap,function(el) funBoot(data)) #chaque colonne est une simulation
   
   #on applique la fonction de quantile empirique :
-  quantiles = apply(matBoot,2,function(col) empiricQuantile(col,probs = probs)) 
+  quantiles = apply(matBoot,2,function(col) funparam(col,probs)) 
   #meme ligne : meme quantile
   
   resQuant = apply(quantiles,1,mean)
@@ -70,14 +69,18 @@ basicParam = function(data,NsBootstrap,probs,funBoot)
   return(res)
 }
 
-naifParam = function(data,NsBootstrap,probs)
+naifBootstrap = function(data,NsBootstrap,probs)
 {
+  #bootstrap naif
+  #1a
   res = basicParam(data,NsBootstrap,probs,naif)
   return(res)
 }
 
-smoothParam = function(data,NsBootstrap,probs,sdSmooth = 0.1)
+smoothBoostrap = function(data,NsBootstrap,probs,sdSmooth = 0.1)
 {
+  #smooth bootstrap
+  #1b
   smoothSpecifiq = function(x) 
   {
     smooth(x,sd = sdSmooth)
@@ -88,6 +91,8 @@ smoothParam = function(data,NsBootstrap,probs,sdSmooth = 0.1)
 
 paramBootstrap = function(data,NsBootstrap,probs)
 {
+  #bootstrap parametrique en utilisant Hill pour determiner le parametre
+  #parametrique = 1c
   shape_hat = getParamHill(data)$hat_shape
   paramSpecifiq = function(x) 
   {
@@ -97,42 +102,97 @@ paramBootstrap = function(data,NsBootstrap,probs)
   return(res)
 }
 
-asymptotiqueEst = function(data,probs)
+
+bootstrapModifa = function(data,NsBootstrap,probs)
 {
-  dataSorted = sort(data)
-  n = length(data)
-  nalpha = qnorm(c(.975),0,1)
-  i1 = round(n*probs[1]-sqrt(n)*nalpha*sqrt(probs[1]*(1-probs[1])),0 )
-  i2 = round(n*probs[1]+sqrt(n)*nalpha*sqrt(probs[1]*(1-probs[1])),0 )
-  res = list()
-  res$min = dataSorted[i1]
-  res$max = dataSorted[i2]
+  #2a
+  funBootstrap = function(x,probs)
+  {
+    estShape = getParamHill(x)$hat_shape
+    return(invFun(estShape,probs))
+  }
+  res = basicParam(data,NsBootstrap,probs,naif,funBootstrap)
   return(res)
 }
 
+bootstrapModifb = function(data,NsBootstrap,probs,sdSmooth = log(length(x))/length(x))
+{
+  #2b
+  funBootstrap = function(x,probs)
+  {
+    estShape = getParamHill(x)$hat_shape
+    return(invFun(estShape,probs))
+  }
+  smoothSpecifiq = function(x) 
+  {
+    smooth(x,sd = sdSmooth)
+  }
+  res = basicParam(data,NsBootstrap,probs,smoothSpecifiq,funBootstrap)
+  return(res)
+}
+
+asymptotiqueEst = function(data,probs)
+{
+  #estimateur d'erreur asymptotique, cf formule de Clara
+  #intervalle de confiance 'Clara'
+  res = c()
+  for(p in probs)
+  {
+    dataSorted = sort(data)
+    n = length(data)
+    nalpha = qnorm(c(.975),0,1)
+    i1 = round(n*p-sqrt(n)*nalpha*sqrt(p*(1-p)),0 )
+    i2 = round(n*p+sqrt(n)*nalpha*sqrt(p*(1-p)),0 )
+    res = cbind(res,c(i1,i2))
+  }
+  colnames(res) = probs
+  rownames(res) = c("5%","95%")
+  return(res)
+}
+
+getTheoricBounds = function(data,probs,shape)
+{
+  #intervalles de confiance asymptotique, en connaissant vraiment shape et scale (oracle)
+  #intervalle de confiance 'Matthieu'
+  res = c()
+  for(p in probs)
+  {
+    inter = matrix(c(quantile(data,p)-(p*(1-p))/dpareto(qpareto(p,shape = shape, scale = scale),shape = shape, scale = scale)**2*1.96/sqrt(length(data))
+                     ,quantile(data,p)+(p*(1-p))/dpareto(qpareto(p,shape = shape, scale = scale),shape = shape, scale = scale)**2*1.96/sqrt(length(data))
+    ),nrow = 2)
+    
+    res = cbind(res,inter)
+  }
+  rownames(res) = c("5%","95%")
+  colnames(res) = probs
+  return(res)
+}
+
+
+
 #les donnees
 shape = 2
-x = rpareto(30,shape=shape,scale = scale)+1
+x = rpareto(3000,shape=shape,scale = scale)+1
 
 probs = c(.5,.6,.7,.75,.90,.95,.99)
 qpareto(probs,shape = shape, scale = 1)+1
-naifParam(x,10000,probs)
-smoothParam(x,10000,probs,log(length(x))/length(x) )
-smoothParam(x,10000,probs,1/sqrt(length(x)) )
-paramBootstrap(x,10000,probs,1/sqrt(length(x)) )
-qpareto(probs,shape = shape, scale = 1)+1
+naifBootstrap(x,10000,probs)
+smoothBootstrap(x,10000,probs,log(length(x))/length(x) )
+smoothBootstrap(x,10000,probs,1/sqrt(length(x)) )
+paramBootstrap(x,10000,probs)
+
+bootstrapModifa(x,1000,probs)
+bootstrapModifb(x,1000,probs,log(length(x))/length(x))
 
 
+#il y a probablement un probleme avec Hill ...
 p = .75
 asymptotiqueEst(x,p)
 
-c(quantile(x,p)-(p*(1-p))/dpareto(qpareto(p,shape = shape, scale = scale),shape = shape, scale = scale)**2*1.96/sqrt(length(x))
-  ,quantile(x,p)+(p*(1-p))/dpareto(qpareto(p,shape = shape, scale = scale),shape = shape, scale = scale)**2*1.96/sqrt(length(x))
-)
+getTheoricBounds(x,probs,scale)
 
 
-
-x = rpareto(30000,shape=2,scale =1)+1
+x = rpareto(30,shape=2,scale =1)+1
 getParamEMV(x)
 getParamHill(x,sqrt(length(x)))
 
