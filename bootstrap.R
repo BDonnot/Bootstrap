ce030b6fa9783bbfc56330beeaa23ac682cc1027
Benjamin DONNOT <benjamin.donnot@gmail.com>
2015-05-02 19:07:32 +0200

    re-doing some graphs, cleaning the code

--- bootstrap.R
+++ bootstrap.R
@@ -4,14 +4,14 @@ library(e1071)
 
 ##Plot of the pareto distribution
 xplot = seq(0.001,5,length = 1e5)
-plot(xplot,dpareto(xplot,scale = 1, shape = 1),
+plot(xplot+1,dpareto(xplot,scale = 1, shape = 1),
      xlab = "",
      ylab = "",
      main = "Densite Pareto pour different paremetre de forme",
      type = 'l')
-lines(xplot,dpareto(xplot,scale = 1, shape = 2),col = 'blue')
-lines(xplot,dpareto(xplot,scale = 1, shape = 3),col = 'red')
-lines(xplot,dpareto(xplot,scale = 1, shape = 5),col = 'darkgreen')
+lines(xplot+1,dpareto(xplot,scale = 1, shape = 2),col = 'blue')
+lines(xplot+1,dpareto(xplot,scale = 1, shape = 3),col = 'red')
+lines(xplot+1,dpareto(xplot,scale = 1, shape = 5),col = 'darkgreen')
 legend("topright",
        legend = c("beta = 1","beta = 2","beta = 3","beta = 5"),
        col = c("black","red","blue","darkgreen"),
@@ -43,15 +43,14 @@ empiricQuantile = function(data,probs)
   quantile(data,probs = probs,type = 1)
 }
 
-invFun = function(hat_shape,probs)
+invFun = function(hat_shape,probs,...)
 {
   qpareto(probs,shape = hat_shape, scale = scale)+1
 }
 
-getParamEMV = function(data,kHill)
+getParamEMV = function(data,...)
 {
   #return the parametric estimation of the coefficients shape and scale
-  #kHill unused
   res = list()
   betaHat = 1/mean(log(data))
   res$hat_shape = betaHat
@@ -62,9 +61,17 @@ getParamHill = function(data,kHill = 10)
 {
   k = round(kHill,0)
   dataS = log(sort(data,decreasing = T)[1:k])
+  X_k = dataS[k]
   res = list()
   res$hat_shape = 1/(mean(dataS[1:(k-1)]) - dataS[k])
   return(res)
+}
+
+hillQuantile = function(estShape,probs,data,kHill)
+{
+  dataS = sort(data,decreasing = T)
+  X_k = dataS[kHill]
+  return(X_k + (length(data)/kHill*(1-probs))**(-1/estShape))
 }
 
 ##more evoluate functions
@@ -74,7 +81,7 @@ basicParam = function(data,NsBootstrap,probs,funBoot,funparam=empiricQuantile)
   matBoot = sapply(1:NsBootstrap,function(el) funBoot(data)) #chaque colonne est une simulation
   
   #on applique la fonction de quantile empirique :
-  quantiles = apply(matBoot,2,function(col) funparam(col,probs)) 
+  quantiles = matrix(apply(matBoot,2,function(col) funparam(col,probs)),nrow = length(probs))
   row.names(quantiles) = names(probs)
   #meme ligne : meme quantile
   
@@ -84,6 +91,7 @@ basicParam = function(data,NsBootstrap,probs,funBoot,funparam=empiricQuantile)
   
   plotFun =function(line,main)
   {
+    main = gsub("\\\\","",main)
     hist(line,100,main=main,xlab = "")
     abline(v = quantile(line,probs = probsInter),col = "blue",lty = 2)
   }
@@ -134,42 +142,20 @@ paramBootstrap = function(data,NsBootstrap,probs,funParam = getParamHill,...)
   return(res)
 }
 
-
 bootstrapModifa = function(data,
                            NsBootstrap,
                            probs,
                            funParam = getParamHill,
-                           sdSmooth = log(length(data))/length(data),
+                           funQuantile = invFun,
                            kHill = sqrt(length(data)))
 {
   #2a
   funBootstrap = function(x,probs)
   {
     estShape = funParam(x,kHill)$hat_shape
-    return(invFun(estShape,probs))
+    return(funQuantile(estShape,probs,data,kHill))
   }
   res = basicParam(data,NsBootstrap,probs,naif,funBootstrap)
-  return(res)
-}
-
-bootstrapModifb = function(data,
-                           NsBootstrap,
-                           probs,
-                           funParam = getParamHill,
-                           sdSmooth = log(length(data))/length(data),
-                           kHill = sqrt(length(data)))
-{
-  #2b
-  funBootstrap = function(x,probs)
-  {
-    estShape = funParam(x,kHill)$hat_shape
-    return(invFun(estShape,probs))
-  }
-  smoothSpecifiq = function(x) 
-  {
-    smooth(x,sd = sdSmooth)
-  }
-  res = basicParam(data,NsBootstrap,probs,smoothSpecifiq,funBootstrap)
   return(res)
 }
 
@@ -185,7 +171,7 @@ asymptotiqueEst = function(data,probs)
     nalpha = qnorm(c(.975),0,1)
     i1 = round(n*p-sqrt(n)*nalpha*sqrt(p*(1-p)),0 )
     i2 = round(n*p+sqrt(n)*nalpha*sqrt(p*(1-p)),0 )
-    res = cbind(res,c(data[i1],data[i2]))
+    res = cbind(res,c(dataSorted[i1],dataSorted[i2]))
   }
   result = list()
   result$resQuant = apply(res,2,mean)
@@ -247,7 +233,7 @@ getResults = function(listOfRes)
 #les donnees
 shape = 2
 set.seed(1)
-x = rpareto(30,shape=shape,scale = scale)+1
+x = rpareto(100,shape=shape,scale = scale)+1
 max=1-(1/length(x))
 probs = c(.75,.90,max)
 names(probs) = c("75\\%","90\\%","max")
@@ -265,6 +251,7 @@ getResults(res)
 #smoothBootstrap(x,10000,probs,1/sqrt(length(x)) )
 
 #partie 2
+shape = 2
 set.seed(1)
 x = rpareto(100,shape=shape,scale = scale)+1
 max=1-(1/length(x))
@@ -274,49 +261,67 @@ qpareto(probs,shape = shape, scale = 1)+1
 
 #1c
 resParamEMV = paramBootstrap(data=x,NsBootstrap=10000,probs=probs,funParam=getParamEMV,kHill = NULL)
+scale_hatEMV = getParamEMV(x)
+resAuxEMV = getTheoricBounds(x,probs,scale_hatEMV$hat_shape)
+res = list(resTh,resAuxEMV,resParamEMV)
+names(res) = c("IC Oracle","IC Matthieu avec Beta_hat EMV","IC EMV")
+getResults(res)
+
 resParamHill = paramBootstrap(data=x,NsBootstrap=10000,probs=probs,funParam=getParamHill,kHill = length(x)/3)
+scale_hatHill = getParamHill(x,kHill = length(x)/3)
+resAuxHill = getTheoricBounds(x,probs,scale_hatHill$hat_shape)
+res = list(resTh,resAuxHill,resParamHill)
+names(res) = c("IC Oracle","IC Matthieu avec Beta_hat Hill","IC Hill")
+getResults(res)
 
 #2a
-resAEMV = bootstrapModifa(x,1000,probs,funParam=getParamEMV,kHill = NULL)
-resAHill = paramBootstrap(data=x,NsBootstrap=10000,probs=probs,funParam=getParamHill,kHill = length(x)/3)
+resAEMV = bootstrapModifa(data=x,
+                          NsBootstrap = 1000,
+                          probs = probs,
+                          funParam=getParamEMV,
+                          funQuantile = invFun,
+                          kHill = NULL)
+scale_hatEMV = getParamEMV(x)
+resAuxEMV = getTheoricBounds(x,probs,scale_hatEMV$hat_shape)
+res = list(resTh,resAuxEMV,resAEMV)
+names(res) = c("IC Oracle","IC Matthieu avec Beta hat EMV","IC EMV")
+getResults(res)
+
+resAHill = bootstrapModifa(data=x,
+                           NsBootstrap=10000,
+                           probs=probs,
+                           funParam=getParamHill,
+                           funQuantile = hillQuantile,
+                           kHill = length(x)/3)
+scale_hatHill = getParamHill(x,kHill = length(x)/3)
+resAuxHill = getTheoricBounds(x,probs,scale_hatHill$hat_shape)
+res = list(resTh,resAuxHill,resAHill)
+names(res) = c("IC Oracle","IC Matthieu avec Beta hat Hill","IC Hill")
+getResults(res)
 
 
+#partie 2
+set.seed(42)
+probs = c(.90)
+names(probs) = c("90\\%")
 
-
-
-
-
-
-
-
-#les donnees
+shape = 2
 x = rpareto(100,shape=shape,scale = scale)+1
-max=1-(1/length(x))
-probs = c(.75,.90,max)
-names(probs) = c(".75",".90","max")
-qpareto(probs,shape = shape, scale = 1)+1
-naifBootstrap(x,10000,probs)
-
-smoothBoostrap(x,10000,probs,log(length(x))/length(x) )
-smoothBootstrap(x,10000,probs,1/sqrt(length(x)) )
-paramBootstrap(x,10000,probs)
-
-bootstrapModifa(x,1000,probs,kHill=10)
-bootstrapModifa(x,1000,probs,kHill=20)
-bootstrapModifb(x,1000,probs,log(length(x))/length(x))
-
-#il y a probablement un probleme avec Hill ...
-p = .75
-asymptotiqueEst(x,probs)
-
-getTheoricBounds(x,probs,scale)
+resSmooth2 = smoothBootstrap(x,10000,probs,1/sqrt(length(x)) )
+resParamEMV2 = paramBootstrap(data=x,NsBootstrap=10000,probs=probs,funParam=getParamEMV,kHill = NULL)
 
 
-x = rpareto(30,shape=2,scale =1)+1
-getParamEMV(x)
-getParamHill(x,5)
+shape = 3
+x = rpareto(100,shape=shape,scale = scale)+1
+resSmooth3 = smoothBootstrap(x,10000,probs,1/sqrt(length(x)) )
+resParamEMV3 = paramBootstrap(data=x,NsBootstrap=10000,probs=probs,funParam=getParamEMV,kHill = NULL)
 
-# x = rpareto(10000000,shape=12,scale =1)+1
-# betaHat = 1/mean(log(x))
-# betaHat
-# hist(x)
+
+shape = 4
+x = rpareto(100,shape=shape,scale = scale)+1
+resSmooth4 = smoothBootstrap(x,10000,probs,1/sqrt(length(x)) )
+resParamEMV3 = paramBootstrap(data=x,NsBootstrap=10000,probs=probs,funParam=getParamEMV,kHill = NULL)
+
+res = list(resSmooth2,resSmooth3,resSmooth4)
+names(res) = c("beta 2","beta 3","beta 4")
+getResults(res)
